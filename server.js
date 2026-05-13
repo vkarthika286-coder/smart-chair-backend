863c2e7636b437e925a41a61b3c99f1ee5b87fde
Karthika <vkarthika286@gmail.com>
2026-05-13 18:29:43 +0530

    fix render restart

--- server.js
+++ server.js
@@ -213,6 +213,8 @@ const PORT = process.env.PORT || 5000
 app.get("/", (req, res) => {
   res.send("Smart Chair Backend is running")
 })
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT)
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`)
 })
